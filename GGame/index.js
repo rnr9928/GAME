556c648eebae22489b61b01c572b6d938b56a43b
Jang Gyeoungguk <97073355+rnr9928@users.noreply.github.com>
2022-11-07 22:48:28 +0900

    Add files via upload

---
+++ GGame/index.js
@@ -0,0 +1,113 @@
+const canvas = document.querySelector('canvas')
+
+const context = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+
+context.fillStyle = 'white'
+context.fillRect(0,0,canvas.width,canvas.height)
+
+const image = new Image()
+image.src='./img/Pellet Town.png'
+
+const playerImage = new Image()
+playerImage.src = './img/playerDown.png'
+
+class Sprite {
+    constructor({
+        position,
+        velocity,
+        image
+    }) {
+        this.position = position
+        this.image = image
+    }
+    draw() {
+        context.drawImage(this.image,this.position.x,this.position.y)
+    }
+}
+const background = new Sprite({position:{
+    x: -737,
+    y: -590
+},
+image: image
+})
+
+const keys = {
+    w:{
+        pressed: false
+    },
+    a:{
+        pressed: false
+    },
+    s:{
+        pressed: false
+    },
+    d:{
+        pressed: false
+    },
+}
+
+function animate(){
+    window.requestAnimationFrame(animate)
+    // console.log('animate') 
+        background.draw()
+        context.drawImage(
+            playerImage,
+           0,
+           0,
+           playerImage.width/4,
+           playerImage.height,
+            canvas.width/2-playerImage.width/4/2,
+             canvas.height/2-playerImage.height/2,
+             playerImage.width/4,
+             playerImage.height,
+             )
+
+            if (keys.w.pressed && lastKey === 'w') background.position.y+=3
+            else if (keys.a.pressed  && lastKey === 'a')  background.position.x+=3
+            else if (keys.s.pressed  && lastKey === 's')  background.position.y-=3
+            else if (keys.d.pressed  && lastKey === 'd')  background.position.x-=3
+      
+}
+animate()
+let lastKey = ''
+window.addEventListener('keydown',(e)=>{
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed=true
+            lastKey = 'w'
+            break;
+        case 'a':
+            keys.a.pressed=true
+            lastKey = 'a'
+            break;
+        case 's':
+            keys.s.pressed=true
+            lastKey = 's'
+            break;
+        case 'd':
+            keys.d.pressed=true
+            lastKey = 'd'
+            break;
+    }
+})
+
+window.addEventListener('keyup',(e)=>{
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed=false
+            break;
+        case 'a':
+            keys.a.pressed=false
+            break;
+        case 's':
+            keys.s.pressed=false
+            break;
+        case 'd':
+            keys.d.pressed=false
+            break;
+    }
+})
